7a020b9bdae980512c08badb5a04246d163614d3
khanhleg2003 <lythatda942003@gmail.com>
2024-10-23 23:06:42 +0700

    -m

--- src/views/client/components/HeaderClient.vue
+++ src/views/client/components/HeaderClient.vue
@@ -10,45 +10,31 @@
       <nav class="flex space-x-16   ">
         <a href="#" class="text-gray-700 hover:text-green-500">
           Trang chủ
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 inline">
-            <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-          </svg>
+        
         </a>
         <a href="#" class="text-gray-700 hover:text-green-500">
           Tin sách
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 inline">
-            <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-          </svg>
+       
         </a>
         <a href="#" class="text-gray-700 hover:text-green-500">
           Sách hot
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 inline">
-            <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-          </svg>
+          
         </a>
         <router-link to="/author" class="text-gray-700 hover:text-green-500">
           Tác giả
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 inline">
-            <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-          </svg>
+          
         </router-link>
         <a href="#" class="text-gray-700 hover:text-green-500">
           Cuộc thi
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 inline">
-            <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-          </svg>
+      
         </a>
         <a href="#" class="text-gray-700 hover:text-green-500">
           Về BookStore
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 inline">
-            <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-          </svg>
+       
         </a>
         <a href="#" class="text-gray-700 hover:text-green-500">
           Liên hệ
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 inline">
-            <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-          </svg>
+         
         </a>
       </nav>
   
